3a4f916dee52e562577874f9607d9cecc4ded4e6
Jasmin Jessich <jasmin@anw.at>
2017-09-11 00:03:35 +0200

    New script create_all_ubuntu.sh

---
+++ create_all_ubuntu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+prepare_script="./prepare_dkms_src.sh"
+
+function exit_print {
+	if [ -z "${2}" ] ; then
+		code=100
+	else
+		code=${2}
+	fi
+
+	echo "${1}"
+	exit ${code}
+}
+
+function err_exit {
+	exit_print "Error: ${1}" ${2}
+}
+
+function do_prepare_Ubuntu {
+   ${prepare_script} Ubuntu ${1}
+   debuild -S || err_exit "Couldn't create source package for Ubuntu ${1}" 1
+
+   if [ "${2}" = "binary" ]; then
+       debuild -i -us -uc || err_exit "Couldn't create binary package for Ubuntu ${1}" 2
+   fi
+}
+
+# main
+
+do_prepare_Ubuntu precise
+do_prepare_Ubuntu trusty binary
+do_prepare_Ubuntu xenial
+
